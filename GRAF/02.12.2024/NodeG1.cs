dd3476a9e694f2ad32c0594f20368276945b9fc6
BaranIga <153220409+BaranIga@users.noreply.github.com>
2024-12-02 09:00:48 +0100

    Create NodeG1.cs

---
+++ GRAF/02.12.2024/NodeG1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minimalistyczne_drzewo_rozpinajace_grafu
+{
+    internal class NodeG1
+    {
+        public int data;
+
+        public NodeG1(int data)
+        {
+            this.data = data;
+        }
+    }
+}
